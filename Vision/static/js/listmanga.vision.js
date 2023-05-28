b38706e4ab43b49f80899c39d2f4480fcc4859de
Pedro Neto <pedro.rmn19@gmail.com>
2023-05-27 23:11:02 -0300

    Fiz a criação de um filtro de mangas em ordem alfabetica e criei um input para pesquisa de mangas

--- Vision/static/js/listmanga.vision.js
+++ Vision/static/js/listmanga.vision.js
@@ -13,9 +13,10 @@ $(document).ready(function () {
             alert("Ocorreu um erro ao buscar as informações dos mangás.");
         }
     });
-    $(".acard").click(function (event) {
+    $(document).on("click", ".acard", function(event) {
         event.preventDefault();
         var id_manga = $(this).data("item-id");
+        console.log(id_manga)
         var manga = mangas[id_manga];
         var url = window.location.origin;
         $("#MangaBaseName").text(manga.title);
@@ -24,6 +25,39 @@ $(document).ready(function () {
         $("#manga-url-capitulo").attr({
             "href": url + '/manga/' + manga.slug,
             "title": 'Ler ' + manga.title + ' Online!'
+        });
+    });
+////////////////////////////////////////////////////////////////////////////////////
+    $('#manga-name-input').on('input', function() {
+        var notPaginationDiv = $('#not-pagination');
+        var mangaName = $(this).val();
+        if (mangaName.length >= 1) {
+            notPaginationDiv.hide().css('display', 'none');
+        } else {
+            notPaginationDiv.show();
+        }
+        $.ajax({
+            url: '/lista-de-titulos/A-Z/search/',
+            data: {manga_name: mangaName},
+            success: function(data) {
+                var html = '';
+                for (var i = 0; i < data.length; i++) {
+                    var manga = data[i];
+                    html += '<div class="col-my card-manga">';
+                    html += '<div class="card">';
+                    html += '<a data-bs-toggle="modal" data-bs-target="#exampleModal" class="acard" href="#" title="Leia '+ manga.title +' Online!" data-item-id="'+ manga.id_manga +'">';
+                    html += '<img class="card-img-top unic" src="'+ manga.capa +'" alt="'+ manga.title +'">';
+                    html += '</a>';
+                    html += '<div class="card-body">';
+                    html += '<a data-bs-toggle="modal" data-bs-target="#exampleModal" href="#" title="Leia '+ manga.title +' Online!" data-item-id="'+ manga.id_manga +'">';
+                    html += '<h5 class="card-title1">'+ manga.title +'</h5>';
+                    html += '</a>';
+                    html += '</div>';
+                    html += '</div>';
+                    html += '</div>';
+                }
+                $('#search-results').html(html);
+            }
         });
     });
 });
@@ -65,39 +99,4 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
         link.classList.add("active");
     }
-});
-// codigo para pesquisa em tempo real
-$(document).ready(function() {
-    $('#manga-name-input').on('input', function() {
-        var notPaginationDiv = $('#not-pagination');
-        var mangaName = $(this).val();
-        if (mangaName.length >= 1) {
-            notPaginationDiv.hide().css('display', 'none');
-        } else {
-            notPaginationDiv.show();
-        }
-        $.ajax({
-            url: '/lista-de-titulos/A-Z/search/',
-            data: {manga_name: mangaName},
-            success: function(data) {
-                var html = '';
-                for (var i = 0; i < data.length; i++) {
-                    var manga = data[i];
-                    html += '<div class="col-my card-manga">';
-                    html += '<div class="card">';
-                    html += '<a data-bs-toggle="modal" data-bs-target="#exampleModal" class="acard" href="#" title="Leia ' + manga.title + ' Online!" data-item-id="' + manga.id_manga + '">';
-                    html += '<img class="card-img-top unic" src="' + manga.capa + '" alt="' + manga.title + '">';
-                    html += '</a>';
-                    html += '<div class="card-body">';
-                    html += '<a data-bs-toggle="modal" data-bs-target="#exampleModal" href="#" title="Leia ' + manga.title + ' Online!" data-item-id="' + manga.id_manga + '">';
-                    html += '<h5 class="card-title1">' + manga.title + '</h5>';
-                    html += '</a>';
-                    html += '</div>';
-                    html += '</div>';
-                    html += '</div>';
-                }
-                $('#search-results').html(html);
-            }
-        });
-    });
 });
